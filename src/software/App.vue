c21745272ce4c351edadf39d66182154b78a6459
richie0115 <richie920115@gmail.com>
2025-09-28 20:35:01 +0800

    software part

--- src/software/App.vue
+++ src/software/App.vue
@@ -1,241 +1,49 @@
 <template>
     <v-app>
-    <page_loader :loading = 'loading' />
-    
-    <title_nav />
-    
-    <v-main>
-    <sidenav name = 'software' />
-    <v-row justify = end><v-col cols = 12 md = 8 class = 'pa-5'>
-        <v-row class = 'text-start scroller' id = 'Model'>
-            <v-col>
-                <v-card title = 'Model' :variant = 'alpha.card.theme'>
-                    <template v-slot:text>
-                        <div v-html="endzymeToHtml">
-                        </div>
-                    </template>
-                </v-card>
-            </v-col>
-        </v-row>
-        <v-row class = 'text-center scroller' id = 'Software'>
-            <v-col>
-                <v-card title = 'Software' :variant = 'alpha.card.theme'>
-                    <template v-slot:text>
-                        <p class = 'grey-text'>
-                            This is Software.
-                        </p>
-                    </template>
-                </v-card>
-            </v-col>
-        </v-row>
-        <v-row>
-            <v-col cols = 12 md = 6>
-                <v-card title = 'Three enzyme expression constructs' subtitle = 'Design' :variant = 'alpha.card.theme'>
-                    <template v-slot:text>
-                        <!-- <v-row> -->
-                            <v-col>
-                                <v-card title = 'Proteinase K + pTrc99A' color = amber v-ripple>
-                                    <template v-slot:text>
-                                        driven by a lac promoter.
-                                    </template>
-                                </v-card>
-                            </v-col>
-                            
-                            <v-col>
-                                <v-card title = 'Dispersin B + pET28c' color = green v-ripple>
-                                    <template v-slot:text>
-                                        driven by a T7 promoter.
-                                    </template>
-                                </v-card>
-                            </v-col>
+        <page_loader :loading = 'loading' />
+        <title_nav />
 
-                            <v-col>
-                                <v-card title = 'DNase I + pET28c' color = cyan v-ripple>
-                                    <template v-slot:text>
-                                        also driven by a T7 promoter.
-                                    </template>
-                                </v-card>
-                            </v-col>
-                        <!-- </v-row> -->
-                    </template>
-                </v-card>
-            </v-col>
-
-            <v-col cols = 12 md = 6>
-                <v-card title = 'Host strain planning' subtitle = 'Design' :variant = 'alpha.card.theme'>
-                    <v-col>
-                        <v-card :variant = 'alpha.subcard.theme' v-ripple>
+        <v-main>
+        <sidenav name = 'software' />
+            <v-container>
+                <v-row>
+                    <v-col cols="12">
+                        <v-card class="wallpaper-card"
+                            id ='wallpaper'>
                             <template v-slot:text>
-                                The Proteinase K plasmid is transformed into MG1655 λcI857, which carries a temperature-sensitive λ prophage system for heat-induced lysis.
+                                <div class="wallpaper-text">
+                                    A better way to create enzyme
+                                </div>
                             </template>
                         </v-card>
+                        <v-card :variant="alpha.card.theme" class="text-box pa-5 scroll-box">
+                                <div
+                                id="Introduction"
+                                title="Introduction"
+                                class="text-content"
+                                >
+                                <p>
+                                    In our wet-lab experiments, we observed that the effect of enzyme treatment was not as pronounced as expected. This can be attributed to two main factors: 
+                                    (1) enzyme yield, and (2) enzyme activity and binding efficiency. To address the latter, we developed a computational pipeline aimed at optimizing enzyme activity and substrate binding efficiency.
+                                </p>
+
+                                <p>
+                                    The design and optimization of enzymes for specific functions have always been a critical aspect of biotechnology. Traditional methods for enzyme engineering are often labor-intensive, requiring iterative rounds of mutation, expression, and screening. In recent years, the integration of artificial intelligence (AI) and computational biology has revolutionized this process, allowing for the in silico prediction of enzyme structures, stability, and functionality. By combining cutting-edge AI models, molecular modeling tools, and computational docking, this software provides an automated platform for novel enzyme sequence generation and efficiency analysis.
+                                </p>
+
+                                <p>
+                                    The pipeline consists of two major components: 
+                                    (1) a machine learning model, and 
+                                    (2) screening and docking score-based evaluation. 
+                                    First, the machine learning model is employed to generate and optimize protein (enzyme) sequences. These sequences are then subjected to AlphaFold for structural prediction. Next, the predicted enzyme structures are docked with the ligand, followed by screening with a scoring function to evaluate binding performance. Through this pipeline, we are able to identify and obtain a functionally improved sequence within 20 minutes.
+                                </p>
+                                </div>
+                        </v-card>                        
                     </v-col>
-                    <v-col>
-                        <v-card :variant = 'alpha.subcard.theme' v-ripple>
-                            <template v-slot:text>
-                                The Dispersin B and DNase I plasmids are introduced into MG1655 λcI857 ΔlacZ::T7RNAP-kan, since MG1655 does not naturally express T7 polymerase.
-                            </template>
-                        </v-card>
-                    </v-col>
-                </v-card>
-            </v-col>
-        </v-row>
-        
-        <v-row>
-            <v-col cols = 12 md = 6>
-                <v-card title = 'T7 RNA Polymerase integration' subtitle = 'Design' :variant = 'alpha.card.theme'>
-                    <template v-slot:text>
-                        <v-col>
-                            <v-card :variant = 'alpha.subcard.theme' v-ripple>
-                                <template v-slot:text>
-                                    T7 RNA polymerase gene was PCR-amplified from E. coli BL21(DE3).
-                                </template>
-                            </v-card>
-                        </v-col>
-                        <v-col>
-                            <v-card :variant = 'alpha.subcard.theme' v-ripple>
-                                <template v-slot:text>
-                                    A kanamycin resistance cassette was PCR-amplified from KEIO lacY::kan.
-                                </template>
-                            </v-card>
-                        </v-col>
-                        <v-col>
-                            <v-card :variant = 'alpha.subcard.theme' v-ripple>
-                                <template v-slot:text>
-                                    To construct our T7 expression system, we first fused the T7 RNA polymerase (T7RNAP) gene with a kanamycin resistance marker (kanR) using overlapping PCR. This fusion fragment was then integrated into a Red(Cm) strain—provided by our advisor—via Red recombination. Next, we plan to transfer the T7RNAP-kanR cassette into E. coliMG1655 through P1 phage transduction, followed by λ phage infection to establish a lysogenic strain.
-                                </template>
-                            </v-card>
-                        </v-col>
-                    </template>
-                </v-card>
-            </v-col>
-
-            <v-col cols = 12 md = 6>
-                <v-card title = 'Cell lysis strategy' subtitle = 'Design' :variant = 'alpha.card.theme'>
-                    <template v-slot:text>
-                        <v-col>
-                            <v-card :variant = 'alpha.subcard.theme' v-ripple>
-                                <template v-slot:text>
-                                    To ensure safe enzyme release, we integrated a λ prophage with a cI857 temperature-sensitive mutationinto all strains.
-                                </template>
-                            </v-card>
-                        </v-col>
-                        <v-col>
-                            <v-card :variant = 'alpha.subcard.theme' v-ripple>
-                                <template v-slot:text>
-                                    At lower temperatures (e.g., 30°C), IPTG induction activates the lac promoter, driving T7RNAP expression, which in turn drives the expression of T7-promoter-controlled enzymes.
-                                </template>
-                            </v-card>
-                        </v-col>
-                        <v-col>
-                            <v-card :variant = 'alpha.subcard.theme' v-ripple>
-                                <template v-slot:text>
-                                    At 37°C, the λcI857 system is activated, leading to cell lysis and the release of accumulated enzymes into the environment.
-                                </template>
-                            </v-card>
-                        </v-col>
-                    </template>
-                </v-card>
-            </v-col>
-        </v-row>
-    </v-col><v-col cols = 1 /></v-row>
-
-    <v-row justify = end><v-col cols = 12 md = 8 class = 'pa-5'>
-        <v-row><v-col>
-            <v-card :variant = 'alpha.card.theme' class = scroller id = Build>
-                <template v-slot:title>
-                    <div class = text-center>
-                        Build
-                    </div>
-                </template>
-                <template v-slot:text>
-                    In this phase, we designed primers and constructed plasmids using SnapGene. Our plasmids were engineered to express biofilm-disrupting enzymes and include an inducible suicide module for biosafety. Detailed plasmid maps and primer sequences are shown below.
-                    <br><br>
-                    Plasmid
-                    <br>
-                    Primer
-                    <br><br>
-                    This build phase provides the foundation for both enzyme production and controlled cell death, preparing our engineered strains for functional testing in the next stage.
-                </template>
-            </v-card>
-        </v-col></v-row>
-    </v-col><v-col cols = 1 /></v-row>
-
-    <v-row justify = end><v-col cols = 12 md = 8 class = 'pa-5'>
-        <v-row><v-col><v-card id = Test title = 'Test' class = 'text-center scroller' text = 'We plan to evaluate whether our system can effectively produce and release enzymes in a controllable manner, validating its potential for biofilm clearance applications.' :variant = 'alpha.card.theme' /></v-col></v-row>
-        <v-row>
-            <v-col cols = 12 md = 6>
-                <v-card title = 'Enzyme expression testing' :variant = 'alpha.card.theme'>
-                    <template v-slot:text>
-                        <v-col>
-                            <v-card :variant = 'alpha.subcard.theme' v-ripple>
-                                <template v-slot:text>
-                                    Expression of the three enzymes will be confirmed using SDS-PAGE .
-                                </template>
-                            </v-card>
-                        </v-col>
-                        <v-col>
-                            <v-card :variant = 'alpha.subcard.theme' v-ripple>
-                                <template v-slot:text>
-                                    IPTG will be used to induce expression via lac and T7 promoters.
-                                </template>
-                            </v-card>
-                        </v-col>
-                    </template>
-                </v-card>
-            </v-col>
-            <v-col cols = 12 md = 6>
-                <v-card title = 'Lysis system validation' :variant = 'alpha.card.theme'>
-                    <template v-slot:text>
-                        <v-col>
-                            <v-card :variant = 'alpha.subcard.theme' v-ripple>
-                                <template v-slot:text>
-                                    Strains will be grown at 30°C and shifted to 37°C to trigger the cI857 lysis mechanism.
-                                </template>
-                            </v-card>
-                        </v-col>
-                        <v-col>
-                            <v-card :variant = 'alpha.subcard.theme' v-ripple>
-                                <template v-slot:text>
-                                    Cell lysis will be assessed visually and via optical density monitoring.
-                                </template>
-                            </v-card>
-                        </v-col>
-                    </template>
-                </v-card>
-            </v-col>
-            <v-col cols = 12 md = 6>
-                <v-card title = 'Extracellular enzyme release' :variant = 'alpha.card.theme'>
-                    <template v-slot:text>
-                        <v-col>
-                            <v-card :variant = 'alpha.subcard.theme' v-ripple>
-                                <template v-slot:text>
-                                    Supernatants collected post-lysis will be tested for biofilm-degrading activity, verifying that enzymes are released into the medium.
-                                </template>
-                            </v-card>
-                        </v-col>
-                    </template>
-                </v-card>
-            </v-col>
-        </v-row>
-    </v-col><v-col cols = 1 /></v-row>
-
-    <v-row justify = end><v-col cols = 12 md = 8 class = 'pa-5'>
-        <v-row><v-col>
-            <v-card title = 'Testing Area' color = grey>
-                <template v-slot:text>
-                    <v-card title = 'Card theme' variant = outlined>
-                        <v-btn rounded = undefined v-for = 'i in alpha.card.f' :key = 'i' :color = 'alpha.card.theme == i ? `green` : undefined' @click = 'alpha.card.theme = i'> {{ i ? i : 'NORMAL' }} </v-btn>
-                    </v-card>
-                    <v-card title = 'Sub-Card theme' variant = outlined>
-                        <v-btn rounded = undefined v-for = 'i in alpha.subcard.f' :key = 'i' :color = 'alpha.subcard.theme == i ? `green` : undefined' @click = 'alpha.subcard.theme = i'> {{ i ? i : 'NORMAL' }} </v-btn>
-                    </v-card>
-                </template>
-            </v-card>
-        </v-col></v-row>
-    </v-col><v-col cols = 1 /></v-row>
-
-    </v-main></v-app>
+                </v-row>
+            </v-container>
+        </v-main>
+    </v-app>
     
     <site_footer></site_footer>
 </template>
@@ -318,4 +126,48 @@ export default {
 a.unfocused, a.unfocused:visited, a.unfocused:hover, a.unfocused:active {
     color: inherit;
 }
+#wallpaper{
+    background-image: url('https://static.igem.wiki/teams/6003/software/endzyme-logo.webp');
+}
+.wallpaper-card {
+    height: 25vw;
+    width: 65vw;
+    background-color: transparent !important;
+    box-shadow: none !important;
+    background-position: center;
+    background-size: cover; /* cover / contain / auto */
+    background-repeat:space;
+    background-position: center;
+}
+
+.wallpaper-text {
+    position: absolute;
+    top:20vw;
+    left:19vw;           
+    transform: translateX(-50%); 
+    color: black;
+    font-size: 2rem;      
+}
+.text-box {
+  width: 100%;
+  max-width: 900px;  
+  margin: 0 auto;    
+}
+.scroll-box {
+  max-height: 600vh;      
+  overflow-y: auto;   
+}
+
+.scroll-box::-webkit-scrollbar {
+  width: 6px;
+}
+.text-content {
+  font-size: 18px;      
+  line-height: 1.6;    
+  color: #333;     
+}
+.text-content p {
+  margin-bottom: 1.5em;
+}
+
 </style>
